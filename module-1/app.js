89f792011e34eed4c776ef5721f274ddc4081624
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-11-26 23:47:56 -0600

    output a method

--- module-1/app.js
+++ module-1/app.js
@@ -5,5 +5,15 @@ const app = Vue.createApp({
       vueLink: 'https://vuejs.org/',
     }
   },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random()
+      if (randomNumber < 0.5) {
+        return 'Learn Vue!'
+      } else {
+        return 'Master the universe.'
+      }
+    },
+  },
 })
 app.mount('#user-goal')
