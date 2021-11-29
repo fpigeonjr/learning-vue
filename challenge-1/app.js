e26ba4fbd1b9764a3f740895f8fabd1943bac1f0
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-11-29 14:21:48 -0600

    lesson 1 challenge

---
+++ challenge-1/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      me: {
+        name: 'Frank Pigeon',
+        age: 42,
+      },
+      imgLink:
+        'https://images.unsplash.com/photo-1551415923-a2297c7fda79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
+      max: 1,
+    }
+  },
+  methods: {
+    getRandomInt(max) {
+      return Math.floor(Math.random() * max)
+    },
+  },
+})
+app.mount('#assignment')
