2375a40a5b4e745bf7969e99b1ea8fe1baa09662
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-11-30 15:49:12 -0600

    refactors to use vue methods - adds reset button

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-    };
+    }
   },
-});
+  methods: {
+    add() {
+      this.counter++
+    },
+    reduce() {
+      this.counter--
+    },
+    reset() {
+      this.counter = 0
+    },
+  },
+})
 
-app.mount('#events');
+app.mount('#events')
