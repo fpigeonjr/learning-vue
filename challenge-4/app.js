efb5b3ce43399994967fef0dc34d25e93ee90cc8
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-03 17:05:27 -0600

    refactors to computed methods for classes

--- challenge-4/app.js
+++ challenge-4/app.js
@@ -6,6 +6,16 @@ const app = Vue.createApp({
       bgColor: '',
     }
   },
+  computed: {
+    paraClasses() {
+      return {
+        user1: this.className === 'user1',
+        user2: this.className === 'user2',
+        visible: !this.isHidden,
+        hidden: this.isHidden,
+      }
+    },
+  },
   methods: {
     toggleHide() {
       this.isHidden = !this.isHidden
