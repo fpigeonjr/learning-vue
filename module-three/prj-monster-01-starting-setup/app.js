42cc11051354beba8296d406d7309cbcc9e24b64
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-07 17:08:25 -0600

    adds game over screen

--- module-three/prj-monster-01-starting-setup/app.js
+++ module-three/prj-monster-01-starting-setup/app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     }
   },
   computed: {
@@ -19,6 +20,26 @@ const app = Vue.createApp({
     },
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // draw
+        this.winner = 'draw'
+      } else if (value <= 0) {
+        // player lost
+        this.winner = 'monster'
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // draw
+        this.winner = 'draw'
+      } else if (value <= 0) {
+        // monster lost
+        this.winner = 'player'
+      }
     },
   },
   methods: {
