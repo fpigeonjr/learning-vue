dd7b7cc1cc00de812ec4dbf967380d147ebf4ca5
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-07 16:49:23 -0600

    adds heal functionality

--- module-three/prj-monster-01-starting-setup/app.js
+++ module-three/prj-monster-01-starting-setup/app.js
@@ -17,9 +17,9 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + '%' }
     },
-    mayUseSpecialAttack(){
+    mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0
-    }
+    },
   },
   methods: {
     attackMonster() {
@@ -36,6 +36,16 @@ const app = Vue.createApp({
       this.currentRound++
       const attackValue = getRandomValue(10, 25)
       this.monsterHealth -= attackValue
+      this.attackPlayer()
+    },
+    healPlayer() {
+      this.currentRound++
+      const healValue = getRandomValue(8, 20)
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100
+      } else {
+        this.playerHealth += healValue
+      }
       this.attackPlayer()
     },
   },
