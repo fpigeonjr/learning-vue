054ad8cd0ae6baf5b22b9029ad4df0ae626d0e51
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-09 12:43:42 -0600

    adds FriendContact comp

--- vue-cli/vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli/vue-cli-01-a-new-vue-project/src/App.vue
@@ -2,7 +2,8 @@
   <section>
     <h2>My Friends</h2>
     <ul>
-      <li></li>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
     </ul>
   </section>
 </template>
@@ -13,19 +14,19 @@ export default {
     return {
       friends: [
         {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "0123 45678 90",
-          email: "manuel@localhost.com",
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '0123 45678 90',
+          email: 'manuel@localhost.com',
         },
         {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "0987 654421 21",
-          email: "julie@localhost.com",
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '0987 654421 21',
+          email: 'julie@localhost.com',
         },
       ],
-    };
+    }
   },
-};
+}
 </script>
