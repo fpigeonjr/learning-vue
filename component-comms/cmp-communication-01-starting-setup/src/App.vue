3d05395ef8aaa550f72985e515d076d0cca5ba03
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-10 08:10:44 -0600

    adds fav prop

--- component-comms/cmp-communication-01-starting-setup/src/App.vue
+++ component-comms/cmp-communication-01-starting-setup/src/App.vue
@@ -8,11 +8,13 @@
         name="Manuel Lorenz"
         phone-number="0123 45678 90"
         email-address="manuel@localhost.com"
+        is-favorite="1"
       ></friend-contact>
       <friend-contact
         name="Julie Jones"
         phone-number="0987 654421 21"
-      email-address="julie@localhost.com"
+        email-address="julie@localhost.com"
+        is-favorite="0"
       ></friend-contact>
     </ul>
   </section>
--- component-comms/cmp-communication-01-starting-setup/src/components/FriendContact.vue
+++ component-comms/cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -1,6 +1,7 @@
 <template>
   <li>
-    <h2>{{ name }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : '' }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
     </button>
@@ -19,7 +20,7 @@
 
 <script>
 export default {
-  props: ['name', 'phoneNumber', 'emailAddress'],
+  props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
   data() {
     return {
       detailsAreVisible: false,
@@ -29,11 +30,19 @@ export default {
         phone: '0123 45678 90',
         email: 'manuel@localhost.com',
       },
+      friendIsFavorite: this.isFavorite,
     }
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible
+    },
+    toggleFavorite() {
+      if (this.friendIsFavorite === '1') {
+        this.friendIsFavorite = '0'
+      } else {
+        this.friendIsFavorite = '1'
+      }
     },
   },
 }
