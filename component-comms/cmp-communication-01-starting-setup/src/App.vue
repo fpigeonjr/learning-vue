574ff0cf0caf8acee44c3dfadd3f293a3997c9ad
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-10 15:13:00 -0600

    adds dynamic prop values

--- component-comms/cmp-communication-01-starting-setup/src/App.vue
+++ component-comms/cmp-communication-01-starting-setup/src/App.vue
@@ -5,16 +5,12 @@
     </header>
     <ul>
       <friend-contact
-        name="Manuel Lorenz"
-        phone-number="0123 45678 90"
-        email-address="manuel@localhost.com"
-        is-favorite="1"
-      ></friend-contact>
-      <friend-contact
-        name="Julie Jones"
-        phone-number="0987 654421 21"
-        email-address="julie@localhost.com"
-        is-favorite="1"
+        v-for="friend in friends"
+        :key="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="true"
       ></friend-contact>
     </ul>
   </section>
--- component-comms/cmp-communication-01-starting-setup/src/components/FriendContact.vue
+++ component-comms/cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : '' }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite ? '(Favorite)' : '' }}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
@@ -35,21 +35,14 @@ export default {
       required: true,
     },
     isFavorite: {
-      type: String,
+      type: Boolean,
       required: false,
-      default: '0',
-      validator: (value) => value === '1' || value === '0',
+      default: false,
     },
   },
   data() {
     return {
       detailsAreVisible: false,
-      friend: {
-        id: 'manuel',
-        name: 'Manuel Lorenz',
-        phone: '0123 45678 90',
-        email: 'manuel@localhost.com',
-      },
       friendIsFavorite: this.isFavorite,
     }
   },
@@ -58,11 +51,7 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible
     },
     toggleFavorite() {
-      if (this.friendIsFavorite === '1') {
-        this.friendIsFavorite = '0'
-      } else {
-        this.friendIsFavorite = '1'
-      }
+      this.friendIsFavorite = !this.friendIsFavorite
     },
   },
 }
