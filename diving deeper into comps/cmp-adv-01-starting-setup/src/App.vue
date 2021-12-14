c20e66b942ed1c132e6b2e981a2145b8f46fc231
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-14 17:17:18 -0600

    adds scoped slots

--- diving deeper into comps/cmp-adv-01-starting-setup/src/App.vue
+++ diving deeper into comps/cmp-adv-01-starting-setup/src/App.vue
@@ -7,6 +7,10 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     />
+    <CourseGoals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps['another-prop'] }}</p>
+    </CourseGoals>
   </div>
 </template>
 
@@ -14,12 +18,14 @@
 import TheHeader from './components/TheHeader.vue'
 import UserInfo from './components/UserInfo.vue'
 import BadgeList from './components/BadgeList.vue'
+import CourseGoals from './components/CourseGoals.vue'
 
 export default {
   components: {
     TheHeader,
     UserInfo,
     BadgeList,
+    CourseGoals,
   },
   data() {
     return {
