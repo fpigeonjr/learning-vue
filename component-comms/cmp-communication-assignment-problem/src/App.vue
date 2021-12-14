331265f47dbf9903e6866f0120c00d75f89e2b44
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-13 19:31:11 -0600

    refactors to use an object

--- component-comms/cmp-communication-assignment-problem/src/App.vue
+++ component-comms/cmp-communication-assignment-problem/src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <user-data @update-user="updateUser"></user-data>
-  <active-user :name="name" :age="age"></active-user>
+  <active-user :name="user.name" :age="user.age"></active-user>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      name: '',
-      age: 0,
+      user: {
+        name: '',
+        age: 0,
+      },
     }
   },
   methods: {
     updateUser(enteredName, enteredAge) {
-      this.name = enteredName
-      this.age = enteredAge
+      this.user = {
+        name: enteredName,
+        age: enteredAge,
+      }
     },
   },
 }
--- component-comms/cmp-communication-assignment-problem/src/components/UserData.vue
+++ component-comms/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -24,7 +24,7 @@ export default {
       enteredName: '',
       enteredAge: '',
     }
-  },
+  }, 
   methods: {
     handleSubmit() {
       this.$emit('update-user', this.enteredName, this.enteredAge)
