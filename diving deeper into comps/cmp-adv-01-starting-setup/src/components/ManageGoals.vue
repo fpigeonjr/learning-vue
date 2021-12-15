9a4e6719b1336433de5b94f4190dc36925468b7d
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-14 18:35:17 -0600

    cleanup

--- diving deeper into comps/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ diving deeper into comps/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,16 +1,14 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal" />
-    <button @click="setGoal">Set Goal</button>
-    <teleport to="body">
-      <ErrorAlert v-if="inputIsInvalid">
-        <h2>Input is invalid</h2>
-        <p>Please fix your input</p>
-        <button @click="confirmError">Ok</button>
-      </ErrorAlert>
-    </teleport>
-  </div>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <teleport to="body">
+    <ErrorAlert v-if="inputIsInvalid">
+      <h2>Input is invalid</h2>
+      <p>Please fix your input</p>
+      <button @click="confirmError">Ok</button>
+    </ErrorAlert>
+  </teleport>
 </template>
 
 <script>
