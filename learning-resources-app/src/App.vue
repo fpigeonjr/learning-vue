7945ea8c5f87a1614dd0c0b0c6b44e58ad266c10
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-14 20:14:03 -0600

    adds resource list and item

---
+++ learning-resources-app/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <StoredResources :resources="storedResources" />
+</template>
+
+<script>
+import StoredResources from './components/resources/StoredResources.vue';
+
+export default {
+  components: {
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ learning-resources-app/src/components/resources/StoredResources.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <ResourceItem
+      v-for="resource in resources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    />
+  </ul>
+</template>
+
+<script>
+import ResourceItem from './ResourceItem.vue';
+
+export default {
+  components: {
+    ResourceItem,
+  },
+  props: ['resources'],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
