b7407eb59a5c8c57a4d7adb896299b6d8991efe4
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-16 21:25:30 -0600

    handles no data

--- sending-http-requests/http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ sending-http-requests/http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -8,7 +8,10 @@
         >
       </div>
       <p v-if="isLoading">Loading...</p>
-      <ul v-else>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">
+        No stored experiences found. Add survey results first.
+      </p>
+      <ul v-else-if="!isloading && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
