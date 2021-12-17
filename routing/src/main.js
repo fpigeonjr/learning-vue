a1241f40f68d8a816bb8d5abe65d1150d258babc
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-12-17 14:45:11 -0600

    adds vue-router

--- routing/src/main.js
+++ routing/src/main.js
@@ -1,7 +1,17 @@
 import { createApp } from 'vue';
-
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
-const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+  ],
+});
 
+const app = createApp(App);
+app.use(router);
 app.mount('#app');
