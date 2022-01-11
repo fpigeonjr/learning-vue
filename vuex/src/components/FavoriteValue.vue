d50c9befc7dc10fd0e05ef558114929356367ca8
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-01-10 20:50:31 -0600

    adds getters

--- vuex/src/components/FavoriteValue.vue
+++ vuex/src/components/FavoriteValue.vue
@@ -1,12 +1,13 @@
 <template>
   <h3>{{ counter }}</h3>
+  <p>we do more stuff</p>
 </template>
 
 <script>
 export default {
   computed: {
     counter() {
-      return this.$store.getters.finalCounter;
+      return this.$store.getters.normalizedCounter;
     },
   },
 };
