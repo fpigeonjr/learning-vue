5edf8b1ad240f082de18e47b7ecba7b28ff86dac
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-01-10 19:45:58 -0600

    starts vuex module

---
+++ vuex/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <base-container title="Vuex">
+    <TheCounter />
+    <button @click="addOne">Add 1</button>
+    <change-counter />
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+  methods: {
+    addOne() {
+      this.$store.state.counter = this.$store.state.counter + 11;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
